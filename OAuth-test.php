3c1b3903d0364427f6ce3964a671862cebb656d1
zhenias <76514552+liquidatorrs@users.noreply.github.com>
2022-08-13 02:03:14 +0200

    Create OAuth-test.php

---
+++ OAuth-test.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="pl-PL">
+  <head>
+    <meta charset="UTF-8">
+    <title>Friendschool OAuth authorize</title>
+    <?php
+      $client_id = '[client_id]';
+      $redirect_uri = '[redirect_uri]';
+      
+    ?>
+    <script src="/js/api/openapi.js"></script>
+    <script>
+      var oauth = {
+        client_id: '[YOURS_CLIENT_ID]',
+        scope: 'email,login,id,drive_key',
+        redirect_uri: '[YOURS_REDIRECT_URI]',
+        display: 'popup',
+      };
+
+      function loginFs() {
+        FS.auth({
+          client_id: oauth.client_id,
+          scope: oauth.scope,
+          redirect_uri: oauth.redirect_uri,
+          display: oauth.display,
+        });
+      }
+
+    </script>
+
+  </head>
+  <body>
+
+        <a onclick="loginFs()">Zaloguj się przez Friendschool</a>
+        <br>
+        <?php
+  $url = 'https://oauth.friendschool.ct8.pl';
+
+  if ($_GET['textNoSuccessOAuth']==true) {
+    echo $_GET['textNoSuccessOAuth'];
+    exit;
+  }
+
+  if (isset($_GET['code'])) {
+    $result = false;
+    
+    $params = http_build_query(array(
+      "client_id" => $client_id,
+      "code" => $_GET['code']
+    ));
+
+    $token = json_decode(file_get_contents($url . '/user/authorize?'.$params), true);
+
+    if ($token['description']) {
+      $userInfo = $token['description'];
+      $result = true;
+    }
+    if ($result) {
+      echo "ID: " . $userInfo['id'] . '<br />';
+      echo "Imię: " . $userInfo['username'] . '<br />';
+      echo "Nazwisko: " . $userInfo['surname'] . '<br />';
+      echo "Login: " . $userInfo['login'] . '<br />';
+      echo "E-mail: " . $userInfo['email'] . '<br />';
+      echo "Klucz dysku: " . $userInfo['drive_key'] . '<br />';
+      echo 'Zdjęcia: <img width="150" src="' . $userInfo['img_profile'] . '" />';
+
+    }
+  }
+
+  ?>
+  </body>
+</html>
